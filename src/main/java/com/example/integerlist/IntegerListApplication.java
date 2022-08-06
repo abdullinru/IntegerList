01123b832087d75c28f4ebacd781bab9115f5611
abdullinru <abdullin17021989@gmail.com>
2022-08-06 23:17:05 +0600

    Initial commit

---
+++ src/main/java/com/example/integerlist/IntegerListApplication.java
@@ -0,0 +1,13 @@
+package com.example.integerlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegerListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/integerlist/IntegerListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integerlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegerListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
